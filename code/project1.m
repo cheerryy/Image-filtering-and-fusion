94482628fb30e91d88c072b1b4d1a4ad364b9e7f
YCui <33620035+cheerryy@users.noreply.github.com>
2020-10-03 11:07:12 +0800

    Rename project1.m to code/project1.m

--- code/project1.m
+++ code/project1.m
@@ -1,19 +1,19 @@
-%�����������ø�˹�˲����õ������в����Ե�ͼƬ�ĸ�Ƶ�͵�Ƶ���֣���������е���
+%本代码是利用高斯滤波器得到两幅有差异性的图片的高频和低频部分，并将其进行叠加
 clear
 clc
-%��ȡͼƬ
+%读取图片
 z1=im2single(imread('cat.bmp'));
 z2=im2single(imread('dog.bmp'));
-sigma=10;%����sigma��ȡֵ��Ƶ������йأ�����Ϊsigma��ֵ����Ƶ�����ֵ
+sigma=10;%由于sigma的取值和频域宽度有关，故认为sigma的值代表频域的阈值
 size=21;
-%ʹ�ø�˹�˲����ӣ���Ӧ���Ǹ�˹��ͨ�˲�������
+%使用高斯滤波算子（对应的是高斯低通滤波操作）
 filter=fspecial('Gaussian',size,sigma);
 low1=myfilter(z1,filter);
 low2=myfilter(z2,filter);
-%��ȡ����һ��ͼƬ�ĸ�Ƶ����
+%提取出第一幅图片的高频部分
 high1=z1-low1;
 high2=z2-low2;
-%��������ͼ��Ļ��
+%进行两幅图像的混合
 output=high1+low2;
 
 imshow(output);
